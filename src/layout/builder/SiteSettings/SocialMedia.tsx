fb742603e0e557a2417a34e8d42ddc449e3b0dce
Glacier <11812405+glacier74@users.noreply.github.com>
2022-11-19 12:21:00 +0800

    Remove enable properties from SiteSetting model

--- src/layout/builder/SiteSettings/SocialMedia.tsx
+++ src/layout/builder/SiteSettings/SocialMedia.tsx
@@ -1,4 +1,4 @@
-import { Input, Switch } from '@heyforms/ui'
+import { Input } from '@heyforms/ui'
 import { useTranslation } from 'next-i18next'
 import { FC, startTransition } from 'react'
 
@@ -6,62 +6,25 @@ import { useSiteSettings } from '~/layout'
 import { SiteSettingsService } from '~/service'
 import { useStore } from '~/store'
 
-const socialMedias = [
-  {
-    valueKey: 'twitter',
-    enabledKey: 'enableTwitter'
-  },
-  {
-    valueKey: 'facebook',
-    enabledKey: 'enableFacebook'
-  },
-  {
-    valueKey: 'instagram',
-    enabledKey: 'enableInstagram'
-  },
-  {
-    valueKey: 'linkedin',
-    enabledKey: 'enableLinkedin'
-  },
-  {
-    valueKey: 'youtube',
-    enabledKey: 'enableYoutube'
-  },
-  {
-    valueKey: 'telegram',
-    enabledKey: 'enableTelegram'
-  },
-  {
-    valueKey: 'github',
-    enabledKey: 'enableGithub'
-  }
+const SOCIAL_MEDIAS = [
+  'twitter',
+  'facebook',
+  'instagram',
+  'linkedin',
+  'youtube',
+  'telegram',
+  'github'
 ] as const
 
-interface SocialMediaItemProps {
-  valueKey: typeof socialMedias[number]['valueKey']
-  enabledKey: typeof socialMedias[number]['enabledKey']
-}
-
-const SocialMediaItem: FC<SocialMediaItemProps> = ({ valueKey, enabledKey }) => {
+const SocialMediaItem: FC<{ name: typeof SOCIAL_MEDIAS[number] }> = ({ name }) => {
   const { t } = useTranslation()
   const settings = useSiteSettings()
   const { updateSiteSettings } = useStore()
 
-  function handleSwitchChange(enabled: boolean) {
-    startTransition(() => {
-      const updates = {
-        [enabledKey]: enabled
-      }
-
-      updateSiteSettings(updates)
-      SiteSettingsService.update(settings.productId, updates)
-    })
-  }
-
   function handleInputChange(value: any) {
     startTransition(() => {
       const updates = {
-        [valueKey]: value
+        [name]: value
       }
 
       updateSiteSettings(updates)
@@ -72,10 +35,9 @@ const SocialMediaItem: FC<SocialMediaItemProps> = ({ valueKey, enabledKey }) => 
   return (
     <div>
       <div className="flex items-center justify-between mb-1">
-        <span className="text-sm text-slate-700">{t(`builder.${valueKey}`)}</span>
-        <Switch value={settings[enabledKey]} onChange={handleSwitchChange} />
+        <span className="text-sm text-slate-700">{t(`builder.${name}`)}</span>
       </div>
-      {settings[enabledKey] && <Input value={settings[valueKey]} onChange={handleInputChange} />}
+      <Input value={settings[name]} onChange={handleInputChange} />
     </div>
   )
 }
@@ -83,8 +45,8 @@ const SocialMediaItem: FC<SocialMediaItemProps> = ({ valueKey, enabledKey }) => 
 export const SocialMedia = () => {
   return (
     <div className="px-4 pt-1 pb-5 space-y-3">
-      {socialMedias.map(row => (
-        <SocialMediaItem key={row.valueKey} {...row} />
+      {SOCIAL_MEDIAS.map(name => (
+        <SocialMediaItem key={name} name={name} />
       ))}
     </div>
   )
