80a36ce4113599d8f0ddf401cab9b8b435daece4
Roy <roebyson@gmail.com>
2022-11-24 15:30:17 +0800

    Update sidebar bg.

--- src/layout/onboarding/OnboardingLayout.tsx
+++ src/layout/onboarding/OnboardingLayout.tsx
@@ -5,7 +5,7 @@ import { LoggedAccount } from './LoggedAccount'
 export function OnboardingLayout({ seo, children }: LayoutProps) {
   return (
     <AuthorizedLayout seo={seo}>
-      <div className="min-h-screen bg-slate-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="min-h-screen bg-amber-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <LoggedAccount />
           {children}
