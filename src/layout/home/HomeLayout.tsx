73993a5bbbd17a080709a06f3af4ddf15a3ed6b6
Glacier <11812405+glacier74@users.noreply.github.com>
2022-12-20 16:19:59 +0800

    Add chatwoot script

--- src/layout/home/HomeLayout.tsx
+++ src/layout/home/HomeLayout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
 import { BaseLayout } from '~/layout'
 
@@ -9,6 +10,26 @@ export function HomeLayout({ seo, children }: LayoutProps): JSX.Element {
         <meta name="theme-color" content="#09092f" />
       </Head>
       <div className="home-container">{children}</div>
+
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(d,t) {
+            var BASE_URL="https://chat.earlybird.im";
+            var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=BASE_URL+"/packs/js/sdk.js";
+            g.defer = true;
+            g.async = true;
+            s.parentNode.insertBefore(g,s);
+            g.onload=function(){
+              window.chatwootSDK.run({
+                websiteToken: 'jiVN6eVixD2uwhy5ac8yKqGH',
+                baseUrl: BASE_URL
+              })
+            }
+          })(document,"script");`
+        }}
+      />
     </BaseLayout>
   )
 }
