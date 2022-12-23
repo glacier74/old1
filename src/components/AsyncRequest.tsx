3c1c6cf6436aca33b28af6a4002b574565c6baf1
Glacier <11812405+glacier74@users.noreply.github.com>
2022-12-23 17:39:20 +0800

    Add reload function to AsyncRequest

--- src/components/AsyncRequest.tsx
+++ src/components/AsyncRequest.tsx
@@ -1,5 +1,5 @@
-import type { FC, ReactNode } from 'react'
-import { useCallback, useEffect, useState } from 'react'
+import type { ReactNode } from 'react'
+import { forwardRef, useCallback, useEffect, useImperativeHandle, useState } from 'react'
 
 export interface AsyncRequestProps extends ComponentProps {
   request: () => Promise<any>
@@ -11,50 +11,67 @@ export interface AsyncRequestProps extends ComponentProps {
   errorRender?: (err: Error) => ReactNode
 }
 
-export const AsyncRequest: FC<AsyncRequestProps> = ({
-  className,
-  style,
-  request,
-  immediate = true,
-  deps = [],
-  cacheFirst = false,
-  skeleton,
-  emptyState,
-  errorRender,
-  children
-}) => {
-  // Fork from https://usehooks.com/useAsync/
-  const [status, setStatus] = useState<'idle' | 'pending' | 'success' | 'error'>('idle')
-  const [result, setResult] = useState<boolean>(false)
-  const [error, setError] = useState<Error | null>(null)
-
-  const execute = useCallback(() => {
-    setStatus('pending')
-    setError(null)
-
-    return request()
-      .then(data => {
-        setResult(data)
-        setStatus('success')
-      })
-      .catch((err: any) => {
-        setError(err)
-        setStatus('error')
-      })
-  }, [request])
-
-  useEffect(() => {
-    if (immediate) {
-      execute()
-    }
-  }, deps.concat(immediate))
-
-  return (
-    <div className={className} style={style}>
-      {status === 'idle' && cacheFirst && children}
-      {status === 'pending' && (!cacheFirst ? skeleton : children)}
-      {status === 'error' && (errorRender ? errorRender(error!) : children)}
-      {status === 'success' && (!result && emptyState ? emptyState : children)}
-    </div>
-  )
+export interface AsyncRequestInstance {
+  reload: () => void
 }
+
+export const AsyncRequest = forwardRef<AsyncRequestInstance, AsyncRequestProps>(
+  (
+    {
+      className,
+      style,
+      request,
+      immediate = true,
+      deps = [],
+      cacheFirst = false,
+      skeleton,
+      emptyState,
+      errorRender,
+      children
+    },
+    ref
+  ) => {
+    // Fork from https://usehooks.com/useAsync/
+    const [status, setStatus] = useState<'idle' | 'pending' | 'success' | 'error'>('idle')
+    const [result, setResult] = useState<boolean>(false)
+    const [error, setError] = useState<Error | null>(null)
+
+    const execute = useCallback(() => {
+      setStatus('pending')
+      setError(null)
+
+      return request()
+        .then(data => {
+          setResult(data)
+          setStatus('success')
+        })
+        .catch((err: any) => {
+          setError(err)
+          setStatus('error')
+        })
+    }, [request])
+
+    useImperativeHandle(ref, () => {
+      return {
+        reload() {
+          execute()
+        }
+      }
+    })
+
+    useEffect(() => {
+      if (immediate) {
+        execute()
+      }
+    }, deps.concat(immediate))
+
+    return (
+      <div className={className} style={style}>
+        {status === 'idle' && cacheFirst && children}
+        {status === 'pending' && (!cacheFirst ? skeleton : children)}
+        {status === 'error' && (errorRender ? errorRender(error!) : children)}
+        {status === 'success' && (!result && emptyState ? emptyState : children)}
+      </div>
+    )
+  }
+)
