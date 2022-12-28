3726802640e5c8305b8a3cc8d5cc6625fd246c29
Glacier <11812405+glacier74@users.noreply.github.com>
2022-12-28 18:00:11 +0800

    Add embed video to HeroSection block

--- src/components/PhotoPicker/index.tsx
+++ src/components/PhotoPicker/index.tsx
@@ -1,6 +1,9 @@
 import { Modal, Tabs } from '@heyforms/ui'
+import { IconVideo } from '@tabler/icons'
 import { useTranslation } from 'next-i18next'
 import type { FC } from 'react'
+
+import { Embed } from '~/components'
 
 import { FileUploader } from '../FileUploader'
 import { Unsplash } from './Unsplash'
@@ -8,9 +11,10 @@ import { Unsplash } from './Unsplash'
 interface PhotoPickerProps extends Omit<ComponentProps, 'onChange'>, IModalProps {
   namespace: string
   enableUnsplash?: boolean
+  enableVideo?: boolean
   acceptedMimes?: string[]
   value?: string
-  onChange?: (value: string) => void
+  onChange?: (value: string, mediaType: 'image' | 'video') => void
 }
 
 export const PhotoPicker: FC<PhotoPickerProps> = ({
@@ -18,6 +22,7 @@ export const PhotoPicker: FC<PhotoPickerProps> = ({
   visible,
   namespace,
   enableUnsplash = false,
+  enableVideo = false,
   acceptedMimes = ['image/jpeg', 'image/png', 'image/bmp'],
   value,
   onClose,
@@ -26,8 +31,13 @@ export const PhotoPicker: FC<PhotoPickerProps> = ({
 }) => {
   const { t } = useTranslation()
 
-  function handleChange(src: string) {
-    onChange?.(src)
+  function handleImageChange(src: string) {
+    onChange?.(src, 'image')
+    onClose?.()
+  }
+
+  function handleVideoChange(src: string) {
+    onChange?.(src, 'video')
     onClose?.()
   }
 
@@ -40,12 +50,30 @@ export const PhotoPicker: FC<PhotoPickerProps> = ({
       {...restProps}
     >
       <Tabs>
-        <Tabs.Pane name="upload" title={t('common.upload')}>
-          <FileUploader namespace={namespace} accept={acceptedMimes} onChange={handleChange} />
+        <Tabs.Pane name="upload" title={t('common.uploadImage')}>
+          <FileUploader namespace={namespace} accept={acceptedMimes} onChange={handleImageChange} />
         </Tabs.Pane>
         {enableUnsplash && (
           <Tabs.Pane name="unsplash" title="Unsplash">
-            <Unsplash onChange={handleChange} />
+            <Unsplash onChange={handleImageChange} />
+          </Tabs.Pane>
+        )}
+        {enableVideo && (
+          <Tabs.Pane name="embed_video" title={t('common.embedVideo')}>
+            <div className="pt-20">
+              <div className="mb-4">
+                <IconVideo className="mx-auto h-12 w-12 text-slate-400 non-scaling-stroke" />
+                <div className="mt-1 text-sm text-slate-600 text-center">
+                  Works with YouTube, Vimeo, and more
+                </div>
+              </div>
+
+              <Embed
+                submitText={t('common.embed')}
+                placeholder="Paste the video link..."
+                onChange={handleVideoChange}
+              />
+            </div>
           </Tabs.Pane>
         )}
       </Tabs>
