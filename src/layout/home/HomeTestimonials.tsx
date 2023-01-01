54e75bbf6d23d6d9a2eaed00a10c0eabc0a0a261
Roy <roebyson+github@gmail.com>
2023-01-01 16:21:33 +0800

    Added real customer testimonials.

--- src/layout/home/HomeTestimonials.tsx
+++ src/layout/home/HomeTestimonials.tsx
@@ -127,6 +127,47 @@ const TestimonialProducthunt: FC<{ testimonial: Testimonial }> = ({ testimonial 
   )
 }
 
+const TestimonialEmail: FC<{ testimonial: Testimonial }> = ({ testimonial }) => {
+  return (
+    <div className="w-full bg-white shadow-md shadow-emerald-200/50 border border-slate-200 p-5 mb-4 rounded-md">
+      <div className="w-full flex items-center">
+        <a href={testimonial.postUrl} target="_blank" rel="noreferrer">
+          <img className="w-12 h-12 rounded-full" src={testimonial.avatar} />
+        </a>
+        <div className="flex-grow pl-3">
+          <div className="flex justify-between my-auto">
+            <div>
+              <h4 className="font-bold inline text-md">
+                  {testimonial.nickname}
+              </h4>
+              <div className="text-sm text-sm text-gray-400 ">{testimonial.title}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full flex-grow mt-2">
+        <div className="text-slate-700 text-md">
+          <p dangerouslySetInnerHTML={{ __html: testimonial.message }} />
+        </div>
+      </div>
+
+      <div className="w-full flex justify-between mt-2">
+        <div className="text-sm text-slate-500 mt-2">
+          <a
+            className="hover:underline"
+            href={testimonial.postUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {testimonial.date}
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const TestimonialItem: FC<{ testimonial: Testimonial }> = ({ testimonial }) => {
   switch (testimonial.type) {
     case 'twitter':
@@ -134,6 +175,9 @@ const TestimonialItem: FC<{ testimonial: Testimonial }> = ({ testimonial }) => {
 
     case 'producthunt':
       return <TestimonialProducthunt testimonial={testimonial} />
+
+      case 'email':
+        return <TestimonialEmail testimonial={testimonial} />
 
     default:
       return null
@@ -150,7 +194,7 @@ export const HomeTestimonials: FC = () => {
           People *really* love EarlyBird
         </h2>
         <div className="max-w-3xl mx-auto text-center text-xl text-slate-700 mt-4">
-          They put in extra effort to express how much they adore crafting their landing pages on
+          They even put in extra effort to express how much they adore crafting their landing pages on
           EarlyBird.
         </div>
         <div className="relative mt-8 columns-1 sm:columns-3 gap-8">
