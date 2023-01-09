6ea85760ffdb88cea96959ecea0313856db4d1d4
Glacier <11812405+glacier74@users.noreply.github.com>
2023-01-09 15:16:28 +0800

    Add subdomains to sitemap

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -7,7 +7,23 @@ const Sitemap = () => {
 
 export async function getServerSideProps({ res }: NextPageContext): Promise<unknown> {
   const homepage = process.env.NEXT_PUBLIC_HOMEPAGE
-  const html = `<?xml version="1.0" encoding="UTF-8"?>
+  const request = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URI}/sitemap-subdomains?key=${process.env.NEXT_API_VERIFICATION_KEY}`
+  )
+  const result = await request.json()
+
+  let html = result
+    .map(
+      (row: any) =>
+        `<url>
+        <loc>${row.url}</loc>
+        <lastmod>${row.updatedAt}</lastmod>
+        <priority>0.6</priority>
+      </url>`
+    )
+    .join('\n')
+
+  html = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset
     xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -33,6 +49,7 @@ export async function getServerSideProps({ res }: NextPageContext): Promise<unkn
       <lastmod>2022-12-23T10:43:50.748Z</lastmod>
       <priority>0.8</priority>
     </url>
+    ${html}
   </urlset>`
 
   res!.setHeader('Content-Type', 'text/xml')
