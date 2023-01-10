7cfe9d3819a3acfad96f1f778abbab9b9e0aa3a2
Glacier <11812405+glacier74@users.noreply.github.com>
2023-01-10 19:42:34 +0800

    Refactor social login component

--- src/layout/login/SocialLogin.tsx
+++ src/layout/login/SocialLogin.tsx
@@ -23,8 +23,8 @@ const SocialLoginItem: FC<SocialLoginItemProps> = ({ label, icon: Icon, provider
         className="w-full inline-flex justify-center py-2 px-4 border border-slate-300 rounded-md shadow-sm bg-white text-sm font-medium text-slate-500 cursor-pointer hover:bg-slate-50"
         onClick={handleClick}
       >
-        <span className="sr-only">{label}</span>
         <Icon className="w-5 h-5" />
+        <span className="ml-2">{label}</span>
       </div>
     </div>
   )
