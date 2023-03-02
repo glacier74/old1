52b438ca2240a0cdbb529234b2fb2586e53afaf0
Glacier <11812405+glacier74@users.noreply.github.com>
2023-03-02 10:24:21 +0800

    Update link setting

--- src/layout/builder2/rightSidebar/fields/LinkSettingField.tsx
+++ src/layout/builder2/rightSidebar/fields/LinkSettingField.tsx
@@ -1,13 +1,15 @@
-import { Input, Switch } from '@heyforms/ui'
-import { FC } from 'react'
+import { Select, Switch } from '@heyforms/ui'
+import { FC, useMemo } from 'react'
 
-import { useBlockSetting } from '~/layout/builder2/context'
+import { useBlockSetting, useBuilderContext } from '~/layout/builder2/context'
+import { BlockIconText } from '~/layout/builder2/leftSidebar/BlockItem'
 import { getObjectPath } from '~/layout/builder2/utils'
 
 import { SettingField, SettingFieldProps } from './SettingField'
 
 export const LinkSettingField: FC<SettingFieldProps> = ({ schema }) => {
   const { setting, updateSetting } = useBlockSetting<any>(schema.name)
+  const { state } = useBuilderContext()
 
   function handleHrefChange(href: any) {
     updateSetting(href, 'href')
@@ -15,6 +17,32 @@ export const LinkSettingField: FC<SettingFieldProps> = ({ schema }) => {
 
   function handleInNewTabChange(isInNewTab: boolean) {
     updateSetting(isInNewTab, 'isInNewTab')
+  }
+
+  const options = useMemo(() => {
+    return [
+      {
+        type: 'blocks',
+        label: 'Blocks',
+        disabled: true
+      },
+      ...state.blocks
+        .filter(b => b.id === state.selectedBlockId)
+        .map(b => ({
+          value: `#earlybird-block-${b.id}`,
+          type: b.type
+        }))
+    ] as any
+  }, [state.blocks, state.selectedBlockId])
+
+  function optionRender(option: any) {
+    if (option.type === 'blocks') {
+      return <div className="px-2 py-1 pointer-events-none text-gray-500">{option.label}</div>
+    }
+
+    return (
+      <BlockIconText className="flex-1 p-2 cursor-pointer hover:bg-slate-100" type={option.type} />
+    )
   }
 
   return (
@@ -33,11 +61,15 @@ export const LinkSettingField: FC<SettingFieldProps> = ({ schema }) => {
         ))}
       </div>
 
-      <div className="pt-2 flex items-center justify-between">
+      <div>
         <div className="mb-1 text-sm text-gray-700">Link to</div>
-        <Input
-          className="!px-2 !py-1.5"
+        <Select
+          className="builder-setting-link-select"
+          options={options}
           value={setting?.href}
+          optionRender={optionRender}
+          allowInput={true}
+          alwaysInput={true}
           placeholder="Enter link URL, etc, https://example.com"
           onChange={handleHrefChange}
         />
