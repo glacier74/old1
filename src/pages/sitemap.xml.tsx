bf4dde45878ae952ad401c7358282f10a5282316
Glacier <11812405+glacier74@users.noreply.github.com>
2023-04-06 14:30:09 +0800

    Add collection category page to sitemap

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,4 +1,8 @@
 import { NextPageContext } from 'next'
+import { AirtableService } from '~/service/airtable'
+
+const NEXT_AIRTABLE_BASE_ID = process.env.NEXT_AIRTABLE_BASE_ID as string
+const NEXT_AIRTABLE_COLLECTION_ID = process.env.NEXT_AIRTABLE_COLLECTION_ID as string
 
 const Sitemap = () => {
   //
@@ -6,12 +10,18 @@ const Sitemap = () => {
 
 export async function getServerSideProps({ res }: NextPageContext): Promise<unknown> {
   const homepage = process.env.NEXT_PUBLIC_HOMEPAGE
-  const request = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URI}/sitemap-subdomains?key=${process.env.NEXT_API_VERIFICATION_KEY}`
-  )
-  const result = await request.json()
+  const [subdomains, records] = await Promise.all([
+    (await fetch(
+      `${process.env.NEXT_PUBLIC_API_URI}/sitemap-subdomains?key=${process.env.NEXT_API_VERIFICATION_KEY}`
+    )).json(),
+    AirtableService.records<CollectionRecord>(
+      NEXT_AIRTABLE_BASE_ID,
+      NEXT_AIRTABLE_COLLECTION_ID
+    )
+  ])
+  const categories = Array.from(new Set(records.map(r => r.Category)))
 
-  let html = result
+  const subdomainsHTML = subdomains
     .map(
       (row: any) =>
         `<url>
@@ -22,7 +32,18 @@ export async function getServerSideProps({ res }: NextPageContext): Promise<unkn
     )
     .join('\n')
 
-  html = `<?xml version="1.0" encoding="UTF-8"?>
+  const categoriesHTML = categories
+    .map(
+      (row: any) =>
+        `<url>
+        <loc>${homepage}/collections/category/${row.toLowerCase()}</loc>
+        <lastmod>2023-04-05T10:43:50.748Z</lastmod>
+        <priority>0.7</priority>
+      </url>`
+    )
+    .join('\n')
+
+  const html = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset
     xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -48,12 +69,13 @@ export async function getServerSideProps({ res }: NextPageContext): Promise<unkn
       <lastmod>2023-04-05T10:43:50.748Z</lastmod>
       <priority>0.8</priority>
     </url>
+    ${categoriesHTML}
     <url>
       <loc>${homepage}/blog</loc>
       <lastmod>2022-12-23T10:43:50.748Z</lastmod>
       <priority>0.8</priority>
     </url>
-    ${html}
+    ${subdomainsHTML}
   </urlset>`
 
   res!.setHeader('Content-Type', 'text/xml')
